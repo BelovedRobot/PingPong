982341fc7b24af4163c606e6d2b3488205bebecf
Zane Kellogg <zane@belovedrobot.com>
2018-11-24 17:42:51 -0600

    Adding test cases for the datastore

---
+++ PingPong/Example/Tests/TestStashObject.swift
@@ -0,0 +1,16 @@
+//
+//  TestStashObject.swift
+//  PingPong_Example
+//
+//  Created by Zane Kellogg on 11/24/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import PingPong
+
+class TestStashObject : StashObject {
+    @objc var aString : String = ""
+    @objc var aNumber : Int = 0
+    @objc var aDateString : String = ""
+}
